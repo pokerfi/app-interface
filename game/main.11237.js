a8c4814af6c7749ff3c8d49755bf6a7e3dbeea2c
battleman0526 <bouerhj@gmail.com>
2021-11-06 19:13:38 +0800

    update;

--- game/main.11237.js
+++ game/main.11237.js
@@ -134,7 +134,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.26d96.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8e12e.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -172,7 +172,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.7714b.js');
+        require('src/settings.17f43.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -180,7 +180,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.7714b.js');
+        require('src/settings.17f43.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
